7d75bd15fb62d969916fb22b03834c2a165a5e84
pranay-a-1 <pranay.a4795@gmail.com>
2022-10-17 22:20:06 +0100

    added rows

--- netflix reactjs/netflix-build/src/api/Requests.js
+++ netflix reactjs/netflix-build/src/api/Requests.js
@@ -4,13 +4,12 @@ const API_KEY = "4481ecb05fff38edfecac6761b47ca79";
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api_key=S{API_KEY}&language=en-US`,
-    fetchActionMovies : `/discover/movie?api_key=S{API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=S{API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=$(API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=$(API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=$(API_KEY}&with_genres=99`,
-
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies : `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
 
 //https://api.themoviedb.org/3/trending/all/week?api_key=4481ecb05fff38edfecac6761b47ca79&language=en-US
--- netflix reactjs/netflix-build/src/components/Banner.js
+++ netflix reactjs/netflix-build/src/components/Banner.js
@@ -37,12 +37,12 @@ const Banner = () => {
     }}>
 
       <div className='banner__contents'>
-        <h1 className='banner__title'>Movie Name</h1>
+        <h1 className='banner__title'>{movie?.title || movie?.name|| movie?.original_name}</h1>
         <div className='="banner__buttons'>
           <button className='banner__button'>Play</button>
           <button className='banner__button'>My List</button>
         </div>
-        <h1 className='banner__description'>{truncate('This is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description',150)}</h1>
+        <h1 className='banner__description'>{truncate(`${movie?.overview}`,150)}</h1>
       </div>
           <div className='banner--fadeBottom'></div>
     </header>
